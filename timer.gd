c19333d5913b7c57e1f0f58fc14076e6f1531f58
Justin Serrano <justin.serrano1015@gmail.com>
2025-10-26 18:38:30 -0600

    Merge branch 'Anthony-Player-stuff'

---
+++ timer.gd
@@ -0,0 +1,13 @@
+extends Timer
+
+
+@onready var player = $".."
+
+func _ready() -> void:
+	timeout.connect(_on_timeout)
+
+func _on_timeout() -> void:
+	if player.stats.is_running():
+		player.stats.increment_stamina(-30)
+	else:
+		player.stats.increment_stamina(1)
