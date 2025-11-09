d0b23de6c2ca2dc6c0464db86bdd292003aa5a27
aaronm94 <113460100+aaronm94@users.noreply.github.com>
2025-11-09 12:47:33 -0700

    adding flashlight, fog, VHS filter

--- scenes/utilities/button.gd
+++ scenes/utilities/button.gd
@@ -3,6 +3,8 @@ extends MeshInstance3D
 class_name TeleportButton
 
 @export var exit_spawn_point: Node3D
+# Play confetti when the user presses the button
+@onready var confetti_player: AudioStreamPlayer3D = $ConfettiPlayer
 
 func _ready() -> void:
 	if exit_spawn_point == null:
@@ -15,11 +17,18 @@ func interact() -> void:
 		push_warning("%s: no exit_spawn_point set or found" % get_path())
 		return
 
-	# 1) teleport now
-	GameActions.teleport_player_to(exit_spawn_point)
-
-	# 2) tell game_state to use exit spawn point
-	GameState.set_active_spawn_point(exit_spawn_point)
+#	Commented out for now because we dont have multiple maps
+	## 1) teleport now
+	#GameActions.teleport_player_to(exit_spawn_point)
+#
+	## 2) tell game_state to use exit spawn point
+	#GameState.set_active_spawn_point(exit_spawn_point)
+	
+	# --- Play the confetti sound ---
+	if confetti_player and confetti_player.stream:
+		confetti_player.play()
+		# Wait for the sound to finish playing before continuing
+		await confetti_player.finished
 
 func _find_exit_spawn_point() -> Node3D:
 	var nodes := get_tree().get_nodes_in_group("exit_spawn_point")
